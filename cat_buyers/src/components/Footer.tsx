28dfa09239e79aaebf9040ca044a8aae04fcb0c2
Divyesh <143060448+Divyesh-Thakur45@users.noreply.github.com>
2025-06-20 09:48:06 +0530

    Delete cat_buyers directory

--- cat_buyers/src/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-        <div className="bg-gray-100 p-[1%]">
-            <p>Made by <a href="">divyeshthakur370@gmail.com</a></p>
-        </div>
-    )
-}
-
